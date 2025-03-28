72f349d9bd2a4442814495f78eabf5b682523f8c
Nikita <kaska0093@inbox.ru>
2025-03-28 12:39:11 -0700

    Initial Commit

---
+++ amikon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  amikon
+//
+//  Created by Nikita Shestakov on 28.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
